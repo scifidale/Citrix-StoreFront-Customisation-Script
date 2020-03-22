a52048de45d7a65e15e0838fb735202e41594d8a
scifidale-Desktop <49734424+scifidale@users.noreply.github.com>
2020-03-22 20:46:47 +0000

    Update storefront-customisation.ps1
    basic building blocks

--- storefront-customisation.ps1
+++ storefront-customisation.ps1
@@ -1,9 +1,20 @@
+/ Citrix StoreFront customisation guide 
+Invoke-Expression ".\pixie.exe"
+
+Choice.exe /C YN /M "Do you want StoreFront to display the servername? [Y/N]"
+if errorlevel 2 goto :No
+
+$SFVersion = Read-Host -Prompt 
 $colour = "FFFFFF"
 Invoke-Expression ".\pixie.exe"
 $Server = Read-Host -Prompt 'Input your server  name'
-Invoke-Expression ".\pixie.exe"
+/ Invoke-Expression ".\pixie.exe"
 
 
 (Get-Content style.css) | 
 Foreach-Object {$_ -replace '#test',"$colour"}  | 
 Out-File style.css
+
+
+
+:No
