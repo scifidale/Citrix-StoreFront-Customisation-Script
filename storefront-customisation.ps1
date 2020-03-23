dcfb6c5676c893ba1aea2dd2e7b239878e6d936c
scifidale-Desktop <49734424+scifidale@users.noreply.github.com>
2020-03-23 19:15:17 +0000

    basic
    Added script variable and basic tune style.css

--- storefront-customisation.ps1
+++ storefront-customisation.ps1
@@ -5,6 +5,9 @@ Choice.exe /C YN /M "Do you want StoreFront to display the servername? [Y/N]"
 if errorlevel 2 goto :No
 
 $SFVersion = Read-Host -Prompt 
+$ThemeColour = Read-host -Prompt "Input Hex theme Colour one"
+$ButtonColour = Read-Host -Prompt "Input Hex for Button Colour"
+$HeaderColor =  Read-Host -Prompt "Inpurt Hex for post login Header Colour"
 $colour = "FFFFFF"
 Invoke-Expression ".\pixie.exe"
 $Server = Read-Host -Prompt 'Input your server  name'
@@ -18,17 +21,3 @@ Out-File style.css
 
 
 :No
-
-/Logon banner image
-.logon-logo-container {
-    background-image: url(../images/1x/CitrixStoreFront_auth_14B96BFF2B0A6FF8.png);
-}
-
-.web-screen {
-    background-color: #161619;
-    background-image: url(../images/common/ReceiverFullScreenBackground_46E559C0E6B5A27B.jpg);
-    background-size: 100% 100%;
-    min-height: 400px;
-    height: auto!important;
-    height: 400px;
-}
