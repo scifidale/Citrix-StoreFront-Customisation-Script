e9a3a19fa79f51447edf1a30b8fb717ed2bb7409
scifidale-Desktop <49734424+scifidale@users.noreply.github.com>
2020-03-24 22:20:53 +0000

    added choice
    new choice codeblock using if statements

--- storefront-customisation.ps1
+++ storefront-customisation.ps1
@@ -1,15 +1,15 @@
-/ Citrix StoreFront customisation guide 
+/* Citrix StoreFront customisation guide 
 Invoke-Expression ".\pixie.exe"
 
 Choice.exe /C YN /M "Do you want StoreFront to display the servername? [Y/N]"
-if errorlevel 2 goto :No
+if %errorlevel% 2 goto :No
 
 Invoke-Expression ".\pixie.exe"
 $SFStore = Read-Host -Prompt "Input Store name"
 $ThemeColour = Read-host -Prompt "Input Hex theme Colour one"
 $ButtonColour = Read-Host -Prompt "Input Hex for Button Colour"
 $HeaderColor =  Read-Host -Prompt "Inpurt Hex for post login Header Colour"
-$colour = "FFFFFF"
+
 
 $Server = Read-Host -Prompt 'Input your server  name'
 
@@ -19,7 +19,7 @@ $Server = Read-Host -Prompt 'Input your server  name'
 Add-Type -AssemblyName System.Windows.Forms
 $Image = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
 $null = $FileBrowser.ShowDialog()
-$Image | select-object SafefileName | FT -HideTableHeaders
+$Image | select-object FileName | FT -HideTableHeaders
 
 /* Server configuration block for no server name in footer
 
@@ -39,10 +39,29 @@ Out-File style.css
 
 /* UK English Selection
 
-Choice.exe /C YN /M "Do you want StoreFront to display United Kingdom English? [Y/N]"
-if errorlevel 2 goto :NFNUK
+
+$test = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
+if ( $test -eq 'Y') {
+$dale = 'cool'} ELSE {
+$dale = 'not'}
+
 
 robocopy copy "strings.en" "c:\inetpub\wwwroot\Citrix\$SFStore"
 
+:NFNUK
+
+Robocopy $Image "c:\inetpub\wwwroot\Citrix\$SFStore"
 
 :No
+
+
+
+
+
+/* CODE BLOCKS 
+
+$test = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
+if ( $test -eq 'Y') {
+$dale = 'cool'} ELSE {
+$dale = 'not'}
+
