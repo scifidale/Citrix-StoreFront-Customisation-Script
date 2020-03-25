bd92c0888db1f07ad2985456ce46fa19646443e8
scifidale <49734424+scifidale@users.noreply.github.com>
2020-03-25 22:51:07 +0000

    Create script.js

---
+++ Source/script.js
@@ -0,0 +1,33 @@
+﻿// Edit this file to add your customized JavaScript or load additional JavaScript files.
+
+/* Example customization - Show a click through screen on web and native */
+
+/*
+var doneClickThrough = false;
+
+// Before web login
+CTXS.Extensions.beforeLogon = function (callback) {
+    doneClickThrough = true;
+    CTXS.ExtensionAPI.showMessage({
+        messageTitle: "Welcome!",
+        messageText: "Only for WWCo Employees",
+        okButtonText: "Accept",
+        okAction: callback
+    });
+};
+
+// Before main screen (both web and native)
+CTXS.Extensions.beforeDisplayHomeScreen = function (callback) {
+    if (!doneClickThrough) {
+        CTXS.ExtensionAPI.showMessage({
+            messageTitle: "Welcome!",
+            messageText: "Only for WWCo Employees",
+            okButtonText: "Accept",
+            okAction: callback
+        });
+    } else {
+        callback();
+    }
+}; */
+
+/* End of example customization */
