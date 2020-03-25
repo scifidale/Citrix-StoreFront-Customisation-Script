a134851fc0e11ad45844ccfafd3d12c9dea5a83f
scifidale <49734424+scifidale@users.noreply.github.com>
2020-03-25 22:29:25 +0000

    script changes
    correct duplicate

--- storefront-customisation.ps1
+++ storefront-customisation.ps1
@@ -1,4 +1,4 @@
-/* Citrix StoreFront customisation guide 
+# Citrix StoreFront customisation guide 
 Invoke-Expression ".\pixie.exe"
 
 
@@ -10,11 +10,14 @@ $ButtonColour = Read-Host -Prompt "Input Hex for Button Colour"
 $HeaderColor =  Read-Host -Prompt "Inpurt Hex for post login Header Colour"
 $Server = Read-Host -Prompt 'Input your server  name'
 
-$Footer = Read-Host -Prompt "Do you want StoreFront to display the server name in the footer? [Y/N]"
-if ( $Footer -eq 'Y') {
-$CSSsource = 'footer'} ELSE {
-$CSSsource = 'standard'}
-get-content -path footer\script.txt | add-content -path Custom\script.css
+#$Footer = Read-Host -Prompt "Do you want StoreFront to display the server name in the footer? [Y/N]"
+#if ( $Footer -eq 'Y') {
+#$CSSsource = 'footer'} ELSE {
+#$CSSsource = 'standard'}
+#get-content -path footer\script.txt | add-content -path Custom\script.css
+
+Robocopy Source Custom style.css
+Robocopy Source Custom script.js
 
 $Footer = Read-Host -Prompt "Do you want StoreFront to display the server name in the footer? [Y/N]"
 if ( $Footer -eq 'Y') {
@@ -24,13 +27,13 @@ null}
 
 
 
-/* Select Graphic
+#/* Select Graphic
 Add-Type -AssemblyName System.Windows.Forms
 $Image = New-Object System.Windows.Forms.OpenFileDialog -Property @{ InitialDirectory = [Environment]::GetFolderPath('Desktop') }
 $null = $FileBrowser.ShowDialog()
 $Image | select-object FileName | FT -HideTableHeaders
 
-/* Server configuration block for no server name in footer
+#/* Server configuration block for no server name in footer
 
 
 
@@ -46,7 +49,7 @@ Out-File Custom\style.css
 Foreach-Object {$_ -replace '#FFBD16',"$HeaderColour"}  | 
 Out-File Custom\style.css
 
-/* UK English Selection
+#/* UK English Selection
 
 
 $UKE = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
@@ -57,10 +60,10 @@ $UKE = 'nul'}
 
 
 
-/* CODE BLOCKS 
+#/* CODE BLOCKS 
 
-$test = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
-if ( $test -eq 'Y') {
-$dale = 'cool'} ELSE {
-$dale = 'not'}
+#$test = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
+#if ( $test -eq 'Y') {
+#$dale = 'cool'} ELSE {
+#$dale = 'not'}
 
