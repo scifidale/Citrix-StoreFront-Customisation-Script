41ddf4e710f9e834dc5bcb3cc9af4c92f2e442ff
scifidale <49734424+scifidale@users.noreply.github.com>
2020-03-25 14:27:05 +0000

    folder structure
    added structure for base files

---
+++ Custom/strings.en.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $.localization.customStringBundle("en", {
+        Example1: "This is an example",
+        Example2: "This is another example",
+        ActivateReceiver: "",
+	AddedToFavorites: "Remove",
+        AddFavoritesTitle: "Add Favourites",
+        AddFavoritesDescription: "Go to Desktops or Apps, click on Details next to your choice and then select Add to Favourites.",
+        AddFavoritesDescriptionDesktopsOnly: "Go to Desktops, click on Details next to your choice and then select Add to Favourites.",
+        AddFavoritesDescriptionAppsOnly: "Go to Apps, click on Details next to your choice and then select Add to Favourites.",
+        AddFavoritesDescriptionPhone: "Under the menu, go to Desktops or Apps, click on Details next to your choice and then select Add to Favourites.",
+        AddFavoritesDescriptionPhoneDesktopsOnly: "Under the menu, go to Desktops, click on Details next to your choice and then select Add to Favourites.",
+        AddFavoritesDescriptionPhoneAppsOnly: "Under the menu, go to Apps, click on Details next to your choice and then select Add to Favourites.",
+        AddToFavorites: "Add To Favourites",
+        QuickAccess: "Favourites",
+        SearchFavoritesPlaceholder: "Search Favourites",
+    });
+})(jQuery);
--- storefront-customisation.ps1
+++ storefront-customisation.ps1
@@ -1,18 +1,25 @@
 /* Citrix StoreFront customisation guide 
 Invoke-Expression ".\pixie.exe"
 
-Choice.exe /C YN /M "Do you want StoreFront to display the servername? [Y/N]"
-if %errorlevel% 2 goto :No
 
-Invoke-Expression ".\pixie.exe"
+
+
 $SFStore = Read-Host -Prompt "Input Store name"
 $ThemeColour = Read-host -Prompt "Input Hex theme Colour one"
 $ButtonColour = Read-Host -Prompt "Input Hex for Button Colour"
 $HeaderColor =  Read-Host -Prompt "Inpurt Hex for post login Header Colour"
-
-
 $Server = Read-Host -Prompt 'Input your server  name'
 
+$Footer = Read-Host -Prompt "Do you want StoreFront to display the server name in the footer? [Y/N]"
+if ( $Footer -eq 'Y') {
+$CSSsource = 'footer'} ELSE {
+$CSSsource = 'standard'}
+
+
+
+
+/* starting Colour picker applet
+Invoke-Expression ".\pixie.exe"
 
 
 /* Select Graphic
@@ -40,20 +47,10 @@ Out-File style.css
 /* UK English Selection
 
 
-$test = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
-if ( $test -eq 'Y') {
-$dale = 'cool'} ELSE {
-$dale = 'not'}
-
-
-robocopy copy "strings.en" "c:\inetpub\wwwroot\Citrix\$SFStore"
-
-:NFNUK
-
-Robocopy $Image "c:\inetpub\wwwroot\Citrix\$SFStore"
-
-:No
-
+$UKE = Read-Host -Prompt "Do you want StoreFront to display United Kingdom English? [Y/N]"
+if ( $UKE -eq 'Y') {
+Robocopy "Source" Custom strings.en.js} ELSE {
+$UKE = 'nul'}
 
 
 
